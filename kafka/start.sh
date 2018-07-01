542e51553196a8284dbd9f8c2b7a2b86aeda619f
fanyinyi <fanyinyi@sina.com>
2018-06-30 22:53:49 -0400

    ignore kafka

--- kafka/start.sh
+++
@@ -1,3 +0,0 @@
-nohup python produce.py > pro.out &
-echo '-------'
-nohup python consume.py > con.out &
--- kafka/stop.sh
+++
@@ -1,2 +0,0 @@
-ps aux | grep produce | awk '{print $2}' | xargs kill -9
-ps aux | grep consume | awk '{print $2}' | xargs kill -9
